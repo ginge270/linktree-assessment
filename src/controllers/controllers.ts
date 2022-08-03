64088ba8d7b343ac2811055deb193a4b559f898e
ginge270 <63086783+ginge270@users.noreply.github.com>
2022-08-03 12:38:47 +1000

    NEW: add to do files

---
+++ src/controllers/controllers.ts
@@ -0,0 +1,14 @@
+//@to do requests response function controller
+
+export class Controller {
+   
+    //getLinksbyUserId 
+
+    //createLink function
+
+    //updateLink function
+
+    //deleteLink function
+}
+
+//use try catch for validation and error responses 
